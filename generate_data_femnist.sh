c13e7846cc18ecf3f77b97b13e7217ceb9cc69e4
dennis-grinwald <grinwald.dennis@gmail.com>
2024-11-28 08:02:42 +0100

    update femnist data dir gen

--- generate_data_femnist.sh
+++ generate_data_femnist.sh
@@ -7,4 +7,4 @@
 
 cp /home/space/datasets-sqfs/FLIIDNIID/Data.sqfs /tmp/
 
-apptainer run -B /tmp/Data.sqfs:/cluster:image-src=/ --nv fl_bench.sif python generate_data.py -d ${1} -a ${2} -cn ${3} --val_ratio ${4} --test_ratio ${5} --least_samples ${6} --seed ${7}
+apptainer run -B /tmp/Data.sqfs:/cluster:image-src=/ --nv fl_bench.sif python generate_data.py -d ${1}
